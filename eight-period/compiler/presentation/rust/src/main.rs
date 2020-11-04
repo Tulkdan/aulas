3d01442626a0291f1305d593928ba9e75cb739c6
Pedro Correa <pedro8correa@gmail.com>
2020-11-03 22:51:08 -0300

    WIP: rust library for memory usage

--- eight-period/compiler/presentation/rust/src/main.rs
+++ eight-period/compiler/presentation/rust/src/main.rs
@@ -1,8 +1,21 @@
 use std::io::Error;
+use libc::{c_char, c_void};
+use std::ptr::{null, null_mut};
 
 mod util;
 
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+extern "C" fn write_cb(_: *mut c_void, message: *const c_char) {
+    print!("{}", String::from_utf8_lossy(unsafe {
+        std::ffi::CStr::from_ptr(message as *const i8).to_bytes()
+    }));
+}
+
 fn main() -> Result<(), Error> {
+    unsafe { jemalloc_sys::malloc_stats_print(Some(write_cb), null_mut(), null()) };
+
     let names_filename = String::from("names.csv");
     let names_file = util::HelperFile::open(names_filename)?;
 
